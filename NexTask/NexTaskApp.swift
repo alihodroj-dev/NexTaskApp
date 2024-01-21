a9b58555cd58a566a2c49b224d4e592e3c7a1443
Ali Hodroj <aloshito2004@gmail.com>
2024-01-21 23:57:58 +0200

    Initial Commit

---
+++ NexTask/NexTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexTaskApp.swift
+//  NexTask
+//
+//  Created by Ali Hodroj on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct NexTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
